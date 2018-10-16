d86bc73d8aa5ace89ef590329398aa6ec6a88714
Chris <evagorac@bxscience.edu>
2018-10-16 18:54:57 -0400

    almost there

--- main.py
+++ main.py
@@ -11,9 +11,31 @@ for x in range(512):
         cv2.destroyAllWindows()
         sys.exit()
 window = (512,512)
-vert_dist = 3000000 #meters
-mpp = vert_dist/window[1] #metersperpixel
-origin = (256,512)
+vert_dist = 3000000. #meters
+mpp = vert_dist/window[1] * .9 #metersperpixel * percent dist of height
+origin = (window[0]/2,window[1])
+
+#define shape
+shape = np.array([[0,0],[32,64]], np.int32)
+
+maxH = shape[0]
+for x in shape:
+    if x[1] > maxH[1]:
+        maxH = x
+
+
+#define constants
+pos = (0,0) #inital starting position
+rapidity = .8
+
+#equation derived
+def transformPoint(coord):
+    return (-coord[0] * rapidity**2 - rapidity*(coord[0]**2 + coord[1]**2 - rapidity**2 * coord[1]**2)**.5)/(rapidity**2 - 1)
+
+#main loop
+for pixelDist in range(pos,2 * (origin[0] - pos)):
+
+
 
 
 
