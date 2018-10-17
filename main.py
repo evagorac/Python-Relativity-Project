7b4077b688644396a7ff7e413eb6af716206b53c
Chris <evagorac@bxscience.edu>
2018-10-16 20:45:42 -0400

    it is now breaking my computer

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import cv2
 import numpy as np
 import sys
 
+c = 3000000.
 window = (512,512)
-vert_dist = 3000000. #meters
+vert_dist = 1 * c #meters
 mpp = vert_dist/window[1] * .9 #metersperpixel * percent dist of height
 origin = (window[0]/2.,window[1])
 
 #define shape in pixels in normal coordinate space
-shape = np.array([[0,0],[32,64]], np.int32)
+shape = np.array([[0,0],[.1*c,.1*c]], np.int32)
 shape = shape * mpp
 
 #finds lowest point to define what distance refers to
@@ -42,25 +43,33 @@ for vertex in range(len(shape)-1):
 
 #main loop
 #DEFINED IN NORMAL COORDINATE SPACE
-for pixelDisplacement in range(pos[0],int(2 * (origin[0] - pos[0]))):
-    dx = pixelDisplacement * mpp
+for pixelDisplacement in range(pos[0],int(2 * (origin[0] - pos[0])),5):
+    dx = (pixelDisplacement * mpp)/2
     transformed = []
     for x in segmented_values:
         #map point and transform into image display frame
         transformed.append(transformCoordinate(mapPoint((x[0]+dx,x[1]))))
 
     img = np.ones((window[0],window[1])) * 255
+    cv2.polyline(img,transformed,True,(0,255,255),1)
+    cv2.imshow('test',copy)
+    key = cv2.waitKey(2)
+    if (key == ord("q")):
+        cv2.destroyAllWindows()
+        sys.exit()
+
+"""
     for x in range(len(transformed)-1):
         copy = img
-        v1 = transformed[x][0]
-        v2 = transformed[x][1]
-        cv2.line(copy, (v2[1],v1[1]), (v2[0],v1[0]), (0,0,0), 4)
+        v1 = transformed[x]
+        v2 = transformed[x+1]
+        cv2.line(copy, (int(v2[1]),int(v1[1])), (int(v2[0]),int(v1[0])), (0,0,0), 4)
         cv2.imshow('test',copy)
         key = cv2.waitKey(2)
         if (key == ord("q")):
             cv2.destroyAllWindows()
             sys.exit()
-
+"""
 
 
 
