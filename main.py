5a56d8980033e59821120200f48acec8f312fd1b
Chris <evagorac@bxscience.edu>
2018-10-20 13:21:46 -0400

    something works

--- main.py
+++ main.py
@@ -3,40 +3,45 @@ import numpy as np
 import sys
 
 c = 3000 #km/s
-rapidity = .8
+rapidity = .6
 window = (512,512)
 
 #relative to observer
-xRange = (-2*c,2*c)
-yRange = (-2*c,2*c)
+xRange = (-5*c,5*c)
+yRange = (-5*c,5*c)
 
 #define kilometers per pixel in each direction
 xKmpp = (xRange[1] - xRange[0])/window[0]
 yKmpp = (yRange[1] - yRange[0])/window[1]
 
 #always center relative to coordinate axis
-observer = np.array([int(window[0]/2) * xKmpp,int(window[1]/2) * yKmpp])
+observer = np.array([[int(window[0]/2) * xKmpp,int(window[1]/2) * yKmpp]])
 
 #define shape in kilometers in space
-shape = np.array([[0,0],[.25*c,.5*c]], np.int32)
+shape = np.array([[0.,0.],[2*c,3*c]])
 
 #inital starting position in kilometers relative to observer
-startingPos = np.array([-2 * c,c])
+startingPos = np.array([-4*c,4*c])
 
 #defines shape relative to observer
 shape[:] += startingPos[:]
 
 #takes shape relative to observer and fits it into the window
 def transformShape(x):
-    shape = np.copy(x)
-    shape[:,0] /= xKmpp
-    shape[:,1] /= yKmpp
-    shape[:] += observer[:]
-    return shape
+    a = np.copy(x)
+    #print(shape)
+    a[:,0] /= (2 * xKmpp)
+    a[:,1] /= (2 * yKmpp)
+    print("VALUE OF " + str(a))
+    a[:,0] += window[0]/2
+    a[:,1] += window[1]/2
+    #print(shape)
+    #print("\n\n\n")
+    return a
 
 #equation derived
 def mapPoint(coord):
-    return ((-coord[0] * rapidity**2 - rapidity*(coord[0]**2 + coord[1]**2 - rapidity**2 * coord[1]**2)**.5)/(rapidity**2 - 1), coord[1])
+    return ((-coord[0] * rapidity**2 + rapidity*(coord[0]**2 + coord[1]**2 - rapidity**2 * coord[1]**2)**.5)/(rapidity**2 - 1), coord[1])
 
 #SEGMENTED POINTS MADE HERE
 #transforms shape into itself with more verticies to show curves perceived
@@ -56,12 +61,13 @@ img = np.ones((window[0],window[1])) * 255
 
 #main loop
 #TODO fix the main loop like in class
-for dx in range(int(startingPos[0]),int(-startingPos[0]),int(-2 * startingPos[0] / 10)):
+for dx in range(int(startingPos[0]),int(-startingPos[0]),int(-2 * startingPos[0] / 100)):
     #print("pos[0]: {}\npos[1]: {}\ndx: {}".format(pos[0],pos[1],dx))
     perceivedCoords = []
     for x in segmented_values:
         #map point and transform into image display frame
         q = mapPoint((x[0]+dx,x[1]))
+        print(q)
         perceivedCoords.append(mapPoint(q))
 
     transformedCoords = transformShape(np.array(perceivedCoords))
@@ -71,11 +77,11 @@ for dx in range(int(startingPos[0]),int(-startingPos[0]),int(-2 * startingPos[0]
     for x in range(len(transformedCoords)-1):
         v1 = transformedCoords[x]
         v2 = transformedCoords[x+1]
-        print(type(int(v2[1])))
-        print(type(int(v1[1])))
-        print(type(int(v2[0])))
-        print(type(int(v1[0])))
-        cv2.line(copy, int(v2[1]), int(v1[1]), int(v2[0]), int(v1[0]), (0,0,0), 1)
+        print((int(v2[0]) , int(v2[1])))
+        print((int(v1[0]) , int(v1[1])))
+        print("\n")
+        cv2.line(copy, (int(v2[0]) , int(v2[1])) , (int(v1[0]) , int(v1[1])) , (0,0,0) , 2)
+        cv2.line(copy, (int(v2[0]) , int(v2[1])) , (int(v1[0]) , int(v1[1])) , (0,0,0) , 2)
         cv2.imshow('test',copy)
         key = cv2.waitKey(2)
         if (key == ord("q")):
