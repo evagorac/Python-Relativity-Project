601b0a5c947730037b322c1184e54c335c93b5b9
Chris <evagorac@bxscience.edu>
2018-10-20 17:54:18 -0400

    noice

--- main.py
+++ main.py
@@ -3,25 +3,28 @@ import numpy as np
 import sys
 
 c = 3000 #km/s
-rapidity = .8
+rapidity = .9
 window = (512,512)
 
 #relative to observer
-xRange = (-10*c,10*c)
+xRange = (-10*c,5*c)
 yRange = (-5*c,5*c)
 
 #define kilometers per pixel in each direction
 xKmpp = (xRange[1] - xRange[0])/window[0]
 yKmpp = (yRange[1] - yRange[0])/window[1]
 
+hOffset = (xRange[0]+xRange[1])/2/xKmpp
+vOffset = (yRange[0]+yRange[1])/2/yKmpp
+
 #always center relative to coordinate axis
-observer = np.array([[int(window[0]/2) * xKmpp,int(window[1]/2) * yKmpp]])
+observer = np.array([[int(window[0]/2 + hOffset) * xKmpp,int(window[1]/2 + vOffset) * yKmpp]])
 
 #define shape in kilometers in space
-shape = np.array([[0.,0.],[.1*c,-3*c]])
+shape = np.array([[0.,0.],[.5*c,3*c]])
 
 #inital starting position in kilometers relative to observer
-startingPos = np.array([-4*c,-1*c])
+startingPos = np.array([-4*c,1*c])
 
 #defines shape relative to observer
 shape[:] += startingPos[:]
@@ -39,8 +42,8 @@ def transformShape(x):
     return a
 
 #equation derived
-def mapPoint(coord):
-    return ((-coord[0] * rapidity**2 + rapidity*(coord[0]**2 + coord[1]**2 - rapidity**2 * coord[1]**2)**.5)/(rapidity**2 - 1), coord[1])
+def findOffset(coord):
+    return (-coord[0] * rapidity**2 + rapidity*(coord[0]**2 + coord[1]**2 - rapidity**2 * coord[1]**2)**.5)/(rapidity**2 - 1)
 
 #SEGMENTED POINTS MADE HERE
 #transforms shape into itself with more verticies to show curves perceived
@@ -56,19 +59,18 @@ for vertex in range(len(shape)-1):
         #print([x,m*x+b])
 
 img = np.ones((window[0],window[1])) * 255
-cv2.rectangle(img,(int(window[0]/2-4),int(window[1]/2-4)),(int(window[0]/2+4),int(window[1]/2+4)),(0,0,0),2)
+cv2.rectangle(img,(int(window[0]/2-4 - hOffset),int(window[1]/2-4 - vOffset)),(int(window[0]/2+4 - hOffset),int(window[1]/2+4 - vOffset)),(0,0,0),2)
 
 #main loop
 #TODO fix the main loop like in class
 for dx in range(int(startingPos[0] + observer[0][0]),int(-startingPos[0] + observer[0][0]),int(-2 * startingPos[0] / 250)):
-    #print("pos[0]: {}\npos[1]: {}\ndx: {}".format(pos[0],pos[1],dx))
     perceivedCoords = []
     for x in segmented_values:
         #map point and transform into image display frame
-        q = mapPoint((x[0]+dx,x[1]))
+        q = findOffset((x[0]+dx,x[1]))
         print(dx)
-        print("MAPPED POINT WRT OBSERVER: \n" + str(q))
-        perceivedCoords.append(mapPoint(q))
+        print("MAPPED OFFSET WRT OBSERVER: \n" + str(q))
+        perceivedCoords.append((x[0]+q+dx,x[1]))
 
     transformedCoords = transformShape(np.array(perceivedCoords))
     # print("POINT ON WINDOW: \n " + str(transformedCoords) + "\n\n")
@@ -80,8 +82,16 @@ for dx in range(int(startingPos[0] + observer[0][0]),int(-startingPos[0] + obser
         # print((int(v2[0]) , int(v2[1])))
         # print((int(v1[0]) , int(v1[1])))
         # print("\n")
-        #cv2.line(copy, (int(v2[0]) , int(v2[1])) , (int(v1[0]) , int(v1[1])) , (0,0,0) , 2)
         cv2.line(copy, (int(v2[0]) , int(v2[1])) , (int(v1[0]) , int(v1[1])) , (0,0,0) , 2)
+    actualCoords = []
+    for x in shape:
+        actualCoords.append((x[0]+dx,x[1]))
+    actualCoords = transformShape(actualCoords)
+    for x in range(len(actualCoords)-1):
+        v1 = actualCoords[x]
+        v2 = actualCoords[x+1]
+        cv2.line(copy, (int(v2[0]) , int(v2[1])) , (int(v1[0]) , int(v1[1])) , (0,0,0) , 2)
+
     cv2.imshow('test',copy)
     key = cv2.waitKey(2)
     if (key == ord("q")):
